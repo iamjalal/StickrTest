af7d5cbc2f0f7ece4b703683d95063fdb348ce38
Jalal Souky <jalal.souky@isis-papyrus.com>
2014-11-03 18:32:24 +0100

    Not using bitmap mesh anymore! Mayor improvement

--- app/src/main/java/com/test/jalalsouky/stickrtest/view/TransformView.java
+++ app/src/main/java/com/test/jalalsouky/stickrtest/view/TransformView.java
@@ -1,59 +1,40 @@
 package com.test.jalalsouky.stickrtest.view;
 
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by jalalsouky on 01/11/14.
  */
-public class TransformView extends ImageView implements TransformationPointView.onTransformationPointListener {
+public class TransformView extends ImageView {
 
-    private static final int INVALID_POINTER_ID = -1;
-
-    private Bitmap mBitmap;
     private Drawable mDrawable;
 
-    private int mActivePointerId;
+    private Camera mCamera;
+    private Matrix mMatrix;
+
+    private static final int INVALID_POINTER_ID = -1;
+    private int mActivePointerId = INVALID_POINTER_ID;
+
     private float mLastTouchX, mLastTouchY;
+    private float mPosX, mPosY;
+
+    private float mRotationDegrees;
+
+    private float mRotationXAxis;
 
     private ScaleGestureDetector mScaleDetector;
     private float mScaleFactor = 1.f;
 
-    //Translation coordinates
-    private float mX, mY;
-
-    //Transformation coordinates
-    private float mX1, mY1; //Top left
-    private float mX2, mY2; //Top right
-    private float mX3, mY3; //Bottom left
-    private float mX4, mY4; //Bottom right
-
-    //Initial coordinates
-    private float mInitX1, mInitY1,
-                  mInitX2, mInitY2,
-                  mInitX3, mInitY3,
-                  mInitX4, mInitY4;
-
-    private int mState = STATE.UNKNOWN;
-
-    private List<TransformationPointView> mPoints = new ArrayList<TransformationPointView>();
-
-    public class STATE {
-        public static final int UNKNOWN = 0;
-        public static final int MOVE = 1;
-        public static final int SCALE = 2;
-        public static final int ROTATE = 3;
-    }
+    private static final float MAX_SCALE_FACTOR = 5.f;
+    private static final float MIN_SCALE_FACTOR = .25f;
 
     public TransformView(Context context) {
         this(context, null, 0);
@@ -65,89 +46,76 @@ public class TransformView extends ImageView implements TransformationPointView.
 
     public TransformView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
         mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
     }
 
     public void setImageDrawable(Drawable drawable) {
         mDrawable = drawable;
         mDrawable.setBounds(0, 0, mDrawable.getIntrinsicWidth(), mDrawable.getIntrinsicHeight());
-        mBitmap = ((BitmapDrawable)drawable).getBitmap();
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if(mBitmap == null) {
-            return;
-        }
+        mCamera = new Camera();
+        mMatrix = new Matrix();
+
+        float centerX = mDrawable.getBounds().centerX();
+        float centerY = mDrawable.getBounds().centerY();
+
+        mCamera.save();
+        mCamera.rotateX(95);
+        mCamera.getMatrix(mMatrix);
 
         canvas.save();
-
-        int bmW = mBitmap.getWidth() * 3; //Bitmap is too small. Just making it three times bigger
-        int bmH = mBitmap.getHeight() * 3; //Same
-        int cW = getWidth();
-        int cH = getHeight();
-
-        mInitX1 = (cW - bmW) / 2;
-        mInitY1 = (cH - bmH) / 2;
-        mInitX2 = (cW + bmW) / 2;
-        mInitY2 = (cH - bmH) / 2;
-        mInitX3 = (cW - bmW) / 2;
-        mInitY3 = (cH + bmH) / 2;
-        mInitX4 = (cW + bmW) / 2;
-        mInitY4 = (cH + bmH) / 2;
-
-        float[] vertices = {
-                (mInitX1 + mX1 + mX), (mInitY1 + mY1 + mY),
-                (mInitX2 + mX2 + mX), (mInitY2 + mY2 + mY),
-                (mInitX3 + mX3 + mX), (mInitY3 + mY3 + mY),
-                (mInitX4 + mX4 + mX), (mInitY4 + mY4 + mY)
-        };
-
-        canvas.drawBitmapMesh(mBitmap, 1, 1, vertices, 0, null, 0, null);
+        canvas.translate(mPosX, mPosY);
+        canvas.scale(mScaleFactor, mScaleFactor, centerX, centerY);
+        canvas.rotate(mRotationDegrees, centerX, centerY);
+        canvas.concat(mMatrix);
+        mDrawable.draw(canvas);
         canvas.restore();
+
+        mCamera.restore();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-
         mScaleDetector.onTouchEvent(ev);
 
         final int action = ev.getAction();
         switch (action & MotionEvent.ACTION_MASK) {
+
             case MotionEvent.ACTION_DOWN: {
 
-                mState = STATE.UNKNOWN;
+                mActivePointerId = ev.getPointerId(0);
 
-                final float x = ev.getX();
-                final float y = ev.getY();
+                final float x = ev.getX(mActivePointerId);
+                final float y = ev.getY(mActivePointerId);
 
                 mLastTouchX = x;
                 mLastTouchY = y;
-                mActivePointerId = ev.getPointerId(0);
+
                 break;
             }
 
             case MotionEvent.ACTION_MOVE: {
-
                 final int pointerIndex = ev.findPointerIndex(mActivePointerId);
                 final float x = ev.getX(pointerIndex);
                 final float y = ev.getY(pointerIndex);
 
                 if (!mScaleDetector.isInProgress()) {
 
-                    mState = STATE.MOVE;
-
                     final float dx = x - mLastTouchX;
                     final float dy = y - mLastTouchY;
 
-                    mX += dx;
-                    mY += dy;
+                    mPosX += dx;
+                    mPosY += dy;
 
                     invalidate();
                 }
+
+                rotate(ev);
 
                 mLastTouchX = x;
                 mLastTouchY = y;
@@ -156,19 +124,16 @@ public class TransformView extends ImageView implements TransformationPointView.
             }
 
             case MotionEvent.ACTION_UP: {
-                mState = STATE.UNKNOWN;
                 mActivePointerId = INVALID_POINTER_ID;
                 break;
             }
 
             case MotionEvent.ACTION_CANCEL: {
-                mState = STATE.UNKNOWN;
                 mActivePointerId = INVALID_POINTER_ID;
                 break;
             }
 
             case MotionEvent.ACTION_POINTER_UP: {
-                mState = STATE.UNKNOWN;
                 final int pointerIndex = (ev.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK)
                         >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
                 final int pointerId = ev.getPointerId(pointerIndex);
@@ -188,51 +153,26 @@ public class TransformView extends ImageView implements TransformationPointView.
     }
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
-
-            mState = STATE.SCALE;
-
             mScaleFactor *= detector.getScaleFactor();
-            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+            mScaleFactor = Math.max(MIN_SCALE_FACTOR, Math.min(mScaleFactor, MAX_SCALE_FACTOR));
 
             invalidate();
             return true;
         }
     }
 
-    @Override
-    public void onMove(TransformationPointView point) {
+    private void rotate(MotionEvent ev) {
 
-        float dX = point.dX;
-        float dY = point.dY;
+        if(ev.getPointerCount() > 1) {
+            double delta_x = (ev.getX(0) - ev.getX(1));
+            double delta_y = (ev.getY(0) - ev.getY(1));
+            double radians = Math.atan2(delta_y, delta_x);
 
-        switch(point.getType()) {
-            case TransformationPointView.PointType.TOP_LEFT:
-                mX1 += dX;
-                mY1 += dY;
-                break;
-            case TransformationPointView.PointType.TOP_RIGHT:
-                mX2 += dX;
-                mY2 += dY;
-                break;
-            case TransformationPointView.PointType.BOTTOM_LEFT:
-                mX3 += dX;
-                mY3 += dY;
-                break;
-            case TransformationPointView.PointType.BOTTOM_RIGHT:
-                mX4 += dX;
-                mY4 += dY;
-                break;
-            default:
-                return;
+            mRotationDegrees = (float) Math.toDegrees(radians);
+
+            invalidate();
         }
-
-        invalidate();
-    }
-
-    public void addPoint(TransformationPointView point) {
-        mPoints.add(point);
     }
 }
